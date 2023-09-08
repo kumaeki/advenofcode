64816187fa1af7788a7aafa84536af5b50fd6494
KumaEki <35358166+kumaeki@users.noreply.github.com>
2023-09-08 12:19:28 +0000

    [2021/06] [Python] add solution for question 2

--- 06/main.py
+++ 06/main.py
@@ -4,44 +4,35 @@
 #         return 1
 #     return getAmout(6, days - start - 1) + getAmout(8, days - start - 1)
 
-
-
-# closed-form solution
-def getAmout(start, days):
-    n = days - start - 1
-    if n <= 0:
-        return 1
-
-    return (2 ** (n // 8)  + 1) * (2 ** ((n % 8) // 6) )
+# def getAmountOfPoints(file_path, days):
+#     result = 0
+#     with open(file_path, "r") as file:
+#         content = file.readlines()
+#     data = content[0].split(',')
+#     for item in data:
+#         it = int(item)
+#         result = result + getAmout(it, days)
+#     return result
 
 def getAmountOfPoints(file_path, days):
-    result = 0
-
     with open(file_path, "r") as file:
         content = file.readlines()
+    data = content[0].split(',')
+    array = [data.count(str(i)) for i in range(9)]
+    for i in range(days):
+        num = array.pop(0)
+        array[6] += num
+        array.append(num)
 
-    list = content[0].split(',')
-
-    for item in list:
-        it = int(item)
-        result = result + getAmout(it, days)
-        # print("result : {}".format(result))
-
-    print(list)
-    return result
-
+    return sum(array)
 
 result = getAmountOfPoints("input.txt", 18)
 print("result : {}".format(result))
-
 result = getAmountOfPoints("input.txt", 80)
 print("result : {}".format(result))
-
-result = getAmountOfPoints("input.txt", 256)
-print("result : {}".format(result))
-
 result = getAmountOfPoints("input.txt.test2", 80)
 print("result : {}".format(result))
-
+result = getAmountOfPoints("input.txt", 256)
+print("result : {}".format(result))
 result = getAmountOfPoints("input.txt.test2", 256)
 print("result : {}".format(result))
